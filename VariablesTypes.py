9c28a64b38ad8df6a5390dd4d400b4f12110be64
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-20 00:01:25 +0100

    Add files via upload

---
+++ VariablesTypes.py
@@ -0,0 +1,18 @@
+#Every variable in Python is an object.
+# You do not need to declare variables or declare their type before using them
+#********Numbers********
+#python support 2 types of number int and float
+y=50.0 # y is a float
+print("y is a float ,y=",y)
+s=int(y) #y is a float and int()=function used to convert float to int
+print("s became a integer ,s=",s)
+z=60 # z is an integer
+print("z is a int, z=",z)
+k=float(z) #k is a float and float()=function used to convert int to float
+print("k became float ,k=",k)
+
+#************String*************
+#diffrence between simple & double quotes
+name1="double quote don't worry about apostrophes"
+#name2='double quote don't worry about apostrophes # apostrophes terminate the string if using single quotes
+print(name1)
